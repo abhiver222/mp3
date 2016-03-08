218ba0a9489b774932d8c4c03ca8f26360b2bcae
msmi3128 <msmi3128@gmail.com>
2016-03-08 17:07:57 -0600

    everything except list

--- source_js/app.js
+++ source_js/app.js
@@ -2,4 +2,21 @@ var app = angular.module('mp3',['ngRoute']);
 
 app.config(function ($routeProvider) {
 
+	$routeProvider
+		.when('/gallery', {	//when url has gallery in it, we want to load the partial w/ gallery and use democontroller
+			templateUrl : 'partials/gallery.html',
+			controller: 'galleryController'
+		})
+		.when('/list/:rank', {	// angular has ways to search and sort, so dont write custom code
+			templateUrl : 'partials/details.html',
+			controller: 'detailsController'
+		})
+		.when('/list/', {	// angular has ways to search and sort, so dont write custom code
+			templateUrl : 'partials/list.html',
+			controller: 'listController'
+		})
+		.otherwise({
+			redirectTo: '/list'
+		});
+
 })
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,62 @@
 /* Sample Controller */
+app.controller('galleryController', ['$scope', '$http', function($scope, $http) {
+
+    $scope.genres = [ 'Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Musical', 
+                    'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'Western' ];
+
+    $scope.filters = { genre: '' };
+
+    $http.get('data/imdb250.json').success(function(data){
+
+    	$scope.movies = data;
+    	console.log(data);
+
+    }).error(function(err){
+    	console.log(err);
+    })
+
+
+}]);
+
+// LIST view for searching
+app.controller('listController', ['$scope', '$http', function($scope, $http) {
+
+	$scope.myRank = $routeParams.rank;
+
+
+    //append things onto scope object in order for html to recognize it
+    $scope.change = function () {   //add a name to list
+        $scope.classdata.names.push($scope.inputValue);
+        $scope.inputValue = "";
+    }
+
+    $http.get('data/imdb250.json').success(function(data){
+
+        $scope.movies = data;
+        console.log(data);
+
+    }).error(function(err){
+        console.log(err);
+    })
+
+}]);
+
+// DETAILS view for 1 movie at a time
+app.controller('detailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+
+    $http.get('data/imdb250.json').success(function(data){
+        $scope.movie = data[$routeParams.rank - 1];
+        nextRank = +$routeParams.rank+1;
+        prevRank = +$routeParams.rank-1;
+        if(nextRank > 250)
+            nextRank = 1;
+        if(prevRank < 1)
+            prevRank = 250;
+        $scope.nextrank = nextRank;
+        $scope.prevrank = prevRank;
+        console.log(data);
+    }).error(function(err){
+        console.log(err);
+    })
+
+}]);
